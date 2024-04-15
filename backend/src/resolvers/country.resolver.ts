45faac770c09fa36ca32eb18892466b329b958f4
laynaria <pro.laynaria@outlook.fr>
2024-04-15 09:53:19 +0200

    commented resolvers and services with function headers

--- backend/src/resolvers/country.resolver.ts
+++ backend/src/resolvers/country.resolver.ts
@@ -4,16 +4,32 @@ import * as CountryService from "../services/country.service";
 
 @Resolver(Country)
 export class countryResolver {
+  /**
+   *
+   * @returns calls a function in CountryService which returns an Array of Country
+   */
   @Query(() => [Country])
   async getCountries(): Promise<Country[]> {
     return CountryService.findAll();
   }
 
+  /**
+   *
+   * @param code string of country code used to find country by code
+   * @returns calls a function in CountryService which returns a country or an error message if fail
+   */
   @Query(() => Country)
   async getCountryByCode(@Arg("code") code: string): Promise<Country> {
     return CountryService.findByCode(code);
   }
 
+  /**
+   *
+   * @param code string of country code
+   * @param name string of country name
+   * @param emoji string of country emoji
+   * @returns calls a function in CountryService to create a Country in db which returns it
+   */
   @Mutation(() => Country)
   async addCountry(
     @Arg("code") code: string,
--- backend/src/services/country.service.ts
+++ backend/src/services/country.service.ts
@@ -1,13 +1,29 @@
 import { Country } from "../entities/country.entity";
 
+/**
+ *
+ * @returns An array of countries
+ */
 export const findAll = (): Promise<Country[]> => {
   return Country.find();
 };
 
+/**
+ *
+ * @param code string of country code used to find country by code
+ * @returns a country or an error message if fail
+ */
 export const findByCode = (code: string): Promise<Country> => {
   return Country.findOneByOrFail({ code });
 };
 
+/**
+ *
+ * @param code string of country code
+ * @param name string of country name
+ * @param emoji string of country emoji
+ * @returns save a country in database and returns it
+ */
 export const createCountry = async (
   code: string,
   name: string,
